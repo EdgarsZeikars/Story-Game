f9d0bb1dc60a0719ce8c0db99eddf590b20aaa7a
EdgarsZeikars <zeikarsedgars@gmail.com>
2022-11-23 23:10:48 +0200

    Begin on JSON read

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,25 @@
+import org.json.simple.JSONArray;
+import org.json.simple.parser.JSONParser;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 class Main {
-    public static void main(String[] args) {
-        String text = "This is the printed Text. I hope you are happy with it.";
-        String text_bub[] = {"Thank God you are finally here!","I have been waiting for your arrival for like - ages."};
-        ConsoleManager.printText(text);
-        ConsoleManager.printSpeech("Tom",text_bub);
+    public static void main(String[] args) throws Exception {
+        JSONParser parser = new JSONParser();
+
+        String file = "src/ChapterOne.json";
+        String chapter = readFileAsString(file);
+
+        Object obj = parser.parse(chapter);
+        JSONArray data = (JSONArray)obj;
+
+        for (int i = 0; i<data.size(); i++) {
+            Object intentData = data.get(i);
+        }
+    }
+    public static String readFileAsString(String file)throws Exception
+    {
+        return new String(Files.readAllBytes(Paths.get(file)));
     }
 }
